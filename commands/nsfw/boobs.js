02bb0db57b2f368bccc296f861210639328e244f
Aaron <goolyaaron@gmail.com>
2019-08-11 22:54:24 +0100

    Add files via upload

--- commands/nsfw/boobs.js
+++ commands/nsfw/boobs.js
@@ -5,12 +5,22 @@ const rp = require('request-promise-native');
 
 
 exports.run = async (client, message, args, level) => {
+    if (!message.channel.nsfw) return message.channel.send('You can use this command in an NSFW Channel!')
+
+
   return rp.get('http://api.oboobs.ru/boobs/0/1/random').then(JSON.parse).then(function(res)  {
     return rp.get({
         url:'http://media.oboobs.ru/' + res[0].preview,
         encoding: null
     });
 }).then(function(res)   {
-    message.channel.sendFile(res);
+
+const lewdembed = new Discord.RichEmbed()
+      .setTitle("Boobs")
+      .setColor(`#000000`)
+      .setImage("attachment://file.png").attachFiles([{ attachment: res, name: "file.png" }])
+
+
+    message.channel.send(lewdembed);
 });
 }
